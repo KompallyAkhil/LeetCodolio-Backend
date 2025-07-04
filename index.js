aa21658cf935ea6bf45781754d759dd9bcd32763
Akki-luffy <akhilkompally27@gmail.com>
2025-07-04 19:30:15 +0530

    Reading for Deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import puppeteer from 'puppeteer';
 import cors from 'cors';
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.use(cors())
 async function WebScarpe(username) {
     // const browser = await puppeteer.launch({headless:false});
